f7afd055846305ee14f55d71953376e426cb6374
lily <hueduong288@gmail.com>
2025-02-20 08:55:07 +0000

    check alpha of character

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -2,6 +2,8 @@
 
 /**
  * _isalpha - Checks if a character is alpha.
+ * @c: The character to check
+ *
  * Description: Check if c is letter, lowercase or uppercase
  *
  * Return: 1 if c is lowercase, 0 otherwise.
