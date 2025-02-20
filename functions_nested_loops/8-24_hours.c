8394232a79fba8061ffa9007c07028f18757c4b3
lily <hueduong288@gmail.com>
2025-02-20 11:54:47 +0000

    print every minute of the day

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints the every minute of the day
+ *
+ * @n: The number to check
+ * Description: print every minute of the day
+ *
+ * Return: void
+ *
+ */
+
+
+void jack_bauer(void)
+{
+	int h, min;
+
+	h = 0;
+	while (h < 24)
+	{
+		min = 0;
+		while (min < 60)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+
+			min ++;
+		}
+
+		h++;
+	}
+}
