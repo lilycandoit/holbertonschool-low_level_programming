431089de1575669d25d556a1ef1f740b8362b924
lily <hueduong288@gmail.com>
2025-02-21 00:11:17 +0000

    add 2 integers

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * add - add 2 integers
+ *
+ * Description: count 2 params in the argument
+ *
+ * Return: result of 2 int params
+ *
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+
+}
