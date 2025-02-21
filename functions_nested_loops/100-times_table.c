f135a8bbd8c950052f4664b03fc47ebc451e42bf
lily <hueduong288@gmail.com>
2025-02-21 02:57:26 +0000

    print n times table without standard library

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,28 +1,60 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * print_times_table - Prints the n times table, starting from 0
- * @n: The number to generate times table
+ * times_table - to print the n times table
+ * @n: the number of rows and collums
+ *
+ * Description: to print the n times table
+ *
+ * Return: void
+ *
  */
+
 void print_times_table(int n)
 {
 	int row, col, product;
-	
+
 	if (n < 0 || n > 15)
 		return;
-	
-	for (row = 0; row <= n; row++)
+
+	row = 0;
+
+	while (row <= n)
 	{
-		for (col = 0; col <= n; col++)
+		_putchar('0')
+
+		col = 1;
+		while (col < n)
 		{
 			product = row * col;
-			if (col == 0)
-				printf("%d", product);
-			else
-				printf(", %3d", product);
-		}
-		
-		printf("\n");
-	}
-}
+			_putchar(',');
+			_putchar(' ');
 
+
+			if (product < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(product + '0');
+			}
+			else if (product < 100)
+			{
+				_putchar(' ');
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+			else
+			{
+				_putchar((product / 100) + '0');
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+
+			col++;
+		}
+
+		_putchar('\n');
+		row++;
+	}
+
+}
