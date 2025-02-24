3a46a139bca73a503961c7cefc8f9d0469961a18
lily <hueduong288@gmail.com>
2025-02-24 11:50:50 +0000

    print triangle with nested loop

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+
+/**
+ * print_triangle - print triangle with #
+ *
+ * @size: sise of triangle (number of rows)
+ *
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+
+	for (i = 1; i <= size; i++)
+	{
+		/* Print spaces first */
+		for (j = 1; j <= size - i; j ++)
+			_putchar(' ');
+
+
+
+		/* Print '#' for the triangle */
+		for (j = 1; j <= i; j++)
+			_putchar('#');
+
+		_putchar('\n');
+	}
+}
