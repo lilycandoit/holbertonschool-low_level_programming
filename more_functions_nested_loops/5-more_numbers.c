e1d6189ea3ca5f58bafee7946c95f5e8b7a10011
lily <hueduong288@gmail.com>
2025-02-24 04:53:33 +0000

    print 10 times the number

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ *
+ * print_numbers - print numbers from 1 to 14
+ *
+ * Description: To print numbers from 1 to 14
+ *
+ * Return: void
+ *
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 14; i++)
+	{
+		if ( 1 >= 10)
+			_putchar('1');
+
+		_putchar((i % 10) + '0');
+	}
+
+	_putchar('\n');
+}
+
+/**
+ * more_numbers - print 10 times the numbers
+ *
+ * Description: To print the numbers 10 times
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+		print_numbers();
+
+}
