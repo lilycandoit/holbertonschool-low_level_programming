319a388439f1428b745fd053db6c7694af17d289
lily <hueduong288@gmail.com>
2025-02-25 10:40:30 +0000

    print 2nd half of the string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * puts_half - print half of the string
+ *
+ * @str: The string to be printed
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int len, i, start;
+
+	len = 0;
+	while (str[len] != '\0')
+		len++;
+	
+	/* Check the starting index */
+	if (len % 2 == 0)
+		start = len / 2;
+	else
+		start = (len -1) / 2 + 1;
+
+	/*Print 2nd half of the string */
+	for (i = start; str[i] != '\0'; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+
+}
