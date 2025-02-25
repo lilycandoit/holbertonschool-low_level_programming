0a4f472daf23270fbba1a045bbc6e6e97fa88dbd
lily <hueduong288@gmail.com>
2025-02-25 06:30:55 +0000

    print every 2 character of a string

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts2 - print every other character
+ *
+ * @str: The string to be printed
+ *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+
+		i+=2;
+	}
+
+	_putchar('\n');
+}
+
