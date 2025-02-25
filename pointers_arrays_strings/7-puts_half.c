637196cde2828eb6c004a0793967e15dc9f17b9c
lily <hueduong288@gmail.com>
2025-02-25 10:45:50 +0000

    print 2nd half of the string

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -15,12 +15,12 @@ void puts_half(char *str)
 	len = 0;
 	while (str[len] != '\0')
 		len++;
-	
+
 	/* Check the starting index */
 	if (len % 2 == 0)
 		start = len / 2;
 	else
-		start = (len -1) / 2 + 1;
+		start = (len - 1) / 2 + 1;
 
 	/*Print 2nd half of the string */
 	for (i = start; str[i] != '\0'; i++)
