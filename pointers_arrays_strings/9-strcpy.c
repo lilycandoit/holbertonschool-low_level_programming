73c8d44abbee111f57070f627764e697dc45ac74
lily <hueduong288@gmail.com>
2025-02-25 12:55:43 +0000

    copy a pointed string to a pointed string

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,28 @@
+# include "main.h"
+
+/**
+ * _strcpy - copy array one to array b
+ *
+ * @src: the source array which to be copied
+ * @dest: the destination array
+ *
+ * Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	// Check if dest or src is NULL (avoid segmentation fault)
+	if (src == NULL || dest == NULL) 
+	{
+        	printf("Error: NULL pointer detected!\n");
+        	return NULL;
+    	}
+
+	/* This way is copying directly, copy first, then check condition if it reach the ends */
+	while ((src[i] = dest[i]) != '\0')
+		i++;
+
+	return dest;
+}
