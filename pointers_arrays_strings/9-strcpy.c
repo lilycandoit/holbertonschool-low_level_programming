eac08f79c6230279cc0223f18815df602c28a950
lily <hueduong288@gmail.com>
2025-02-26 22:50:50 +0000

    create manual function for copying - interesting one

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -18,13 +18,15 @@ char *_strcpy(char *dest, char *src)
 	/* Check if dest or src is NULL (avoid segmentation fault) */
 	if (src == NULL || dest == NULL)
 	{
-        	printf("Error: NULL pointer detected!\n");
-        	return NULL;
+		printf("Error: NULL pointer detected!\n");
+		return (NULL);
     	}
 
-	/* This way is copying directly, copy first, then check condition if it reach the ends */
+	/** This way is copying directly, copy first,
+	 * then check condition if it reach the ends
+	 */
 	while ((dest[i] = src[i]) != '\0')
 		i++;
 
-	return dest;
+	return (dest);
 }
