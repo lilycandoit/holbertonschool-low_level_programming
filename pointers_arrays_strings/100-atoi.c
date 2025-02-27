53260c2f5051677a82237ecb51d31df0ee4402de
lily <hueduong288@gmail.com>
2025-02-27 03:49:00 +0000

    convert string to number in edge cases

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -17,41 +17,38 @@
 
 int _atoi(char *s)
 {
-  int result = 0; /* the result after converting */
-  int sign = 1; /* initialize for positive number */
-  int found_number = 0; /* false, number is not found initially */
+	int result = 0; /* the result after converting */
+	int sign = 1; /* initialize for positive number */
+	int found_number = 0; /* false, number is not found initially */
 
-  /* iterate through the string using */
-  /* can use *s instead  of s[i] for more readable and eficiency */
-  while (*s)
-  {
-    /* step 1: check for '-' or '+' and get final sign*/
-    if (*s == '-')
-    {
-      sign = sign * (-1);
-    }
-    /* then, check if it is digit number and convert it */
-    else if (*s >= '0' && *s <= '9')
-    {
-      found_number = 1; /* number is found now - will be used for next step */
+	/* iterate through the string using */
+	/* can use *s instead  of s[i] for more readable and eficiency */
+	while (*s)
+	{
+		/* step 1: check for '-' or '+' and get final sign*/
+		if (*s == '-')
+		{
+			sign = sign * (-1);
+		}
+		/* then, check if it is digit number and convert it */
+		else if (*s >= '0' && *s <= '9')
+		{
+			found_number = 1; /* number is found now - will be used for next step */
+			/* prevent overflow with min and max integer */
+			if (result > (INT_MAX / 10) || (result == INT_MAX / 10 && (*s - '0') > (INT_MAX % 10)))
+			{
+				return (sign == 1) ? INT_MAX : INT_MIN;
+			}
 
-      /* prevent overflow with min and max integer */
-      if (result > (INT_MAX / 10) ||
-               (result == INT_MAX / 10 && (*s - '0') > (INT_MAX % 10)))
-            {
-                return (sign == 1) ? INT_MAX : INT_MIN;
-            }
+			/* convert character to digit, result will be acculmulated for next character */
+			result = (result * 10) + (*s - '0');
+		}
+		else if (found_number)
+		{
+			break; /* to ignore the rest after finding the 1st block of digits */
+		}
+		s++; /* move to next character */
+	}
 
-      /* convert character to digit, result will be acculmulated for next character */
-      result = (result * 10) + (*s - '0');
-    }
-    else if (found_number)
-    {
-      break; /* to ignore the rest after finding the 1st block of digits */
-    }
-
-    s++; /* move to next character */
-  }
-
-  return result * sign;
+	return result * sign;
 }
