aa8744941bf601a97677fd2e4f1f461d404bbd6f
lily <hueduong288@gmail.com>
2025-02-27 07:30:34 +0000

    create a function to concatenate 2 strings with pointer

---
+++ pointers_arrays_strings/task0/0-strcat.c
@@ -0,0 +1,33 @@
+#include "../main.h"
+
+/**
+* _strcat - to concatenate two strings
+
+* @dest: the destination string with enough memory
+* @src: the string will be added to dest
+
+* Return: the pointer to the dest string
+*/
+
+char *_strcat(char *dest, char *src)
+{
+  char *ptr = dest; /* save the start of dest */
+
+  /* Find the end of dest string */
+  while (*ptr != '\0')
+    ptr++;
+
+  /* Copy src and add to dest */
+  while (*src != '\0')
+  {
+    /* ptr now is starting from the end of the original dest */
+    *ptr = *src;
+    ptr++;
+    src++;
+  }
+
+  /* after all, add null terminator */
+  *ptr = '\0';
+
+  return dest;
+}
