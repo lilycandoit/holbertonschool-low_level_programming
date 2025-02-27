ede9eb50c981ada1546482e2d4b45aa787b808c9
lily <hueduong288@gmail.com>
2025-02-27 11:58:34 +0000

    create strncat function manually to concatenate 2 strings with n characters

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+* _strncat - to concatenate 2 strings with max n characters
+*
+* @dest: the destination string
+* @src: the string to be appended
+* @n: number of characters added to dest
+*
+* Return: the pointer of string dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *pt = dest; /* save the start of dest */
+	size_t dest_len = 0, i;
+
+	/* find the length of dest*/
+	while (dest[dest_len] != '\0')
+	{
+		dest_len++;
+	}
+
+	/* check and appending src to dest */
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
+		dest[dest_len] = src[i];
+		dest_len++;
+		i++;
+	}
+
+	/* Add null termination to the result */
+	dest[dest_len] = '\0';
+
+	return (dest);
+}
