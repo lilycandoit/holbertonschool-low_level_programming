55faf2d0b6a539883a35b4bd65ff428785a667f0
lily <hueduong288@gmail.com>
2025-02-27 04:18:44 +0000

    print every other character

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -10,15 +10,14 @@
 
 void puts2(char *str)
 {
-	int i;
-
-	i = 0;
+	int i = 0;
 
 	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
+		if (i % 2 == 0)
+			_putchar(str[i]);
 
-		i += 2;
+		i++;
 	}
 
 	_putchar('\n');
