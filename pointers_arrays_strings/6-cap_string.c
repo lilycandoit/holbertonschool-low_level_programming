f1f8277412e7f6c2cfe05ab62a49896a3c591c24
lily <hueduong288@gmail.com>
2025-02-28 07:13:31 +0000

    capitalize the first letter of each word in an array - make betty happy

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -13,39 +13,39 @@
 
 char *cap_string(char *str)
 {
-    int i = 0, j;
-    char separators[] = " \t\n,;.!?\"(){}";
-    int is_start = 1; /* flag to check if it is at start of word */
+	int i = 0, j;
+	char separators[] = " \t\n,;.!?\"(){}";
+    	int is_start = 1; /* flag to check if it is at start of word */
 
-    while (str[i] != '\0')
-    {
-        /* check if the current character is a separator */
-		int is_separator = 0;
-        for (j = 0; separators[j] != '\0'; j++)
-        {
+	while (str[i] != '\0')
+	{
+		/* check if the current character is a separator */
+			int is_separator = 0;
+		for (j = 0; separators[j] != '\0'; j++)
+		{
 			if (str[i] == separators[j])
 			{
 				is_separator = 1; /* if found separator */
 				break; /* then exit loop early*/
 			}
-        }
-
-		if (is_separator)
-		{
-			is_start = 1;
-		}
-		else if (is_start && str[i] >= 'a' && str[i] <= 'z')
-		{
-			str[i] = str[i] - 32; /* convert to uppercase */
-			is_start = 0; /* reset flag after capitalizing */
-		}
-		else
-		{
-			is_start = 0;
 		}
 
-		i++;
-    }
+			if (is_separator)
+			{
+				is_start = 1;
+			}
+			else if (is_start && str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - 32; /* convert to uppercase */
+				is_start = 0; /* reset flag after capitalizing */
+			}
+			else
+			{
+				is_start = 0;
+			}
+
+			i++;
+	}
 
 	return (str);
 }
