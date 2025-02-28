31106d22a90a8951d9e81c232a5c7c63ffe14f62
lily <hueduong288@gmail.com>
2025-02-28 03:33:24 +0000

    create a function to change string to all uppercase

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -8,16 +8,17 @@
 
 char *string_toupper(char *a)
 {
-    int i = 0;
+	int i = 0;
 
-    while (a[i] != '\0')
-    {
-        if (a[i] >= 'a' && a[i] <= 'z')
-        {
-            a[i] -= 32;
-        }
+	while (a[i] != '\0')
+	{
+		if (a[i] >= 'a' && a[i] <= 'z')
+		{
+			a[i] -= 32;
+		}
+
 		i++;
-    }
+	}
 
 	return (a);
 }
