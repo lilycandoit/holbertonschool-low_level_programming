2ebb5e4ec6c84332148ae0555906363a03dda80c
lily <hueduong288@gmail.com>
2025-02-28 01:30:42 +0000

    create a function to revert elements in an array

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+* reverse_array - to reverse content of an array
+*
+* @a: the array with pointer
+* @n: the number of elements of the array
+*
+* Return: void
+*/
+
+void reverse_array(int *a, int n)
+{
+    int i = 0, temp;
+
+    while (i < n / 2) /* n is length of the array */
+    {
+		temp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = temp;
+
+		i++;
+    }
+}
