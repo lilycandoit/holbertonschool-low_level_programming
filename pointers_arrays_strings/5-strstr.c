f8b58be098dd81fd02b3112b3eea6e54b85334b0
lily <hueduong288@gmail.com>
2025-03-03 12:21:52 +0000

    finds the first occurrence of the substring and return a pointer to the beginning of the located substring

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -4,8 +4,8 @@
 /**
 * _strstr - finds the first occurrence of the substring
 *
-* @haystack: the main string
-* @needle: the substring
+* @haystack: the main string to be examined
+* @needle: the substring to be searched
 *
 * Return: a pointer to the beginning of the located substring,
 * or NULL if the substring is not found
@@ -13,22 +13,30 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	char *a;
+	char *h;
+	char *n;
 
-	while (*needle)
+	/* in case needle is empty, return haystack */
+	if (*needle == '\0')
+		return haystack; /* return the full haystack from beginning */
+
+	while (*haystack)
 	{
-		a = haystack;
+		/* need to declare with temp pointer to make sure it is reset */
+		/* after each haystack++ */
+		h = haystack;
+		n = needle;
 
-		while (*a)
+		while (*h && *n && *h == *n) /* step 1: comparing substring */
 		{
-			if (*needle == *a)
-			{
-				/* return the pointer at the point of the first match found */
-				return (needle);
-			}
-			a++;
+			h++;
+			n++;
 		}
-		needle++;
+
+		if (*n == '\0') /* the match is found only when reaching the end of needle*/
+			return haystack;
+
+		haystack++;
 	}
 	return (NULL);
 }
