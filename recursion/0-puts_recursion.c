2f6181ab24ca0e42235468971b2ef6f138e8a336
lily <hueduong288@gmail.com>
2025-03-05 22:48:17 +0000

    create a function to print a string followed by a new line with recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _puts_recursion - to print a string, follow by a new line
+* @s: the string to be printed
+*
+* Return: void
+*/
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(*s); /* print the current character */
+	/* s+1 is similar to s++ without modifying the s */
+	_puts_recursion(s+1);
+}
---
+++ recursion/README.md
@@ -0,0 +1,3 @@
+# This project is to practice about recursion
+
+- There are 7 basics tasks in total
