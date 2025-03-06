c717105da0529d38b57ffb3d0d23a68f041e963c
lily <hueduong288@gmail.com>
2025-03-06 23:01:03 +0000

    prints all arguments it receives

---
+++ argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+
+/**
+* main - to print all arguments it receives
+* @argc: Argument count
+* @argv: Arguments ifselves (array of strings)
+* Return: Always 0 (success)
+*/
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	while (i < argc){
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
