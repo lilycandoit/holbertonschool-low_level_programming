b793ca70bceb88870a43d41592af416463e033f8
lily <hueduong288@gmail.com>
2025-03-06 01:38:02 +0000

    check if a input is prime number - make betty happy

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -21,7 +21,7 @@ int is_prime_number(int n)
 		return (1);
 
 	/* if not base case, return value of the helper function */
-	return check_prime(n, 2);
+	return (check_prime(n, 2));
 }
 
 /**
@@ -43,6 +43,6 @@ int check_prime(int n, int i)
 
 	/* if none of the cases above, then keep calling function */
 	/* to check with (i + 1) */
-	return check_prime(n, i + 1);
+	return (check_prime(n, i + 1));
 }
 
