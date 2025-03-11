da169431f427db632642738e8fd8370d9f5ada73
lily <hueduong288@gmail.com>
2025-03-11 04:34:48 +0000

    create manually _realloc function with malloc and free - make betty happy

--- more_malloc_free/100-realloc.c
+++ more_malloc_free/100-realloc.c
@@ -24,7 +24,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
-		return NULL;
+		return (NULL);
 	}
 
 	/* If ptr is NULL, equivalent to malloc(new_size) */
