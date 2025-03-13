755861f2a034573108fbf81b5a0dc80d1a7b81a6
lily <hueduong288@gmail.com>
2025-03-13 01:58:10 +0000

    create my very first structure in C

---
+++ structures_typedef/README.md
@@ -0,0 +1,3 @@
+# This project is to practice: structures and typedef in C
+
+- There are basic 6 tasks in total
---
+++ structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+* struct dog - a new type named dog
+* @name: first member
+* @age: 2nd member
+* @owner: 3rd member
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
