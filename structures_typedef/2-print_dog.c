124da69a647180f4f933e3df32e299d10f641285
lily <hueduong288@gmail.com>
2025-03-13 11:44:01 +0000

    print the struct

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,29 @@
+#include "dog.h"
+#include "stdio.h"
+#include "string.h"
+
+/**
+* print_dog - to print a struct dog
+* @d: the variable
+* Return: void
+*/
+
+void print_dog(struct dog *d)
+{
+	/* check the struct itself if it is NULL */
+	if (d == NULL)
+		return;
+
+	/* check the child elements */
+	if (d->name == NULL)
+		printf("nil\n");
+	else
+		printf("Name: %s\n", d->name);
+
+	printf("Age: %.2f\n", d->age); /* float is never NULL */
+
+	if (d->owner == NULL)
+		printf("nil\n");
+	else
+		printf("Owner: %s\n", d->owner);
+}
