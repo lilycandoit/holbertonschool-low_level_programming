a47cbc0801552b2c9cc8c9072b31969131b03023
lily <hueduong288@gmail.com>
2025-03-13 09:22:31 +0000

    initialize a variable of a struct

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include "string.h"
+
+/**
+* init_dog - to initialize a variable with struct dog
+* @d: variable name
+* @name: first child element
+* @float: 2nd child element
+* @owner: 3rd child element
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)/* if pointer is not valid */
+	{
+		return;
+	}
+
+	d -> name = name;
+	d -> age = age;
+	d -> owner = owner;
+}
