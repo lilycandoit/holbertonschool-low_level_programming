4a6e0d9b77fc38655c353d7b829ff06a356e8410
lily <hueduong288@gmail.com>
2025-03-17 10:49:28 +0000

    perform simple calculation with command arguments

---
+++ function_pointers/3-main.c
@@ -0,0 +1,50 @@
+#include "3-calc.h"
+#include "stdio.h"
+#include "stdlib.h"
+
+/**
+* main - entry point
+* @argc: number of arguments
+* @argv: array of arguments
+* Return: 0 on success, exits with error code on failure
+*/
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	int (*operation) (int, int);
+
+	/* check if number of arguments is wrong */
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	/* convert input */
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	/* get correct operation function */
+	operation = get_op_func(argv[2]);
+
+	/* if operation is invalid */
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	/* if checking all good, then do calculation */
+	result = operation(num1, num2);
+	printf("%d\n", result);
+
+	return (0);
+}
+
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+/**
+* op_add - get sum
+* @a: integer a
+* @b: integer b
+* Return: the sums of a and b
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+* op_sub - get the difference of a and b
+* @a: integer a
+* @b: integer b
+* Return: the difference of a and b
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+/**
+* op_mul - get the product
+* @a: integer a
+* @b: integer b
+* Return: the product a and b
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+* op_div - get result of division
+* @a: integer a
+* @b: integer b
+* Return: the result of division
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* op_mod - get the remainder of the division
+* @a: integer a
+* @b: integer b
+* Return: the remainder of the division of a by b
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
