ea590f5e092ca7d0f85a1d78702568afe58f0349
lily <hueduong288@gmail.com>
2025-03-17 06:51:04 +0000

    write a function that search for an integer

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include "string.h"
 
 /**
 * int_index - to get the first element matching condition
@@ -15,11 +16,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (size < 0)
 		return (-1);
 
+	/* if array is NULL or cmp is NULL */
+	if (array == NULL || cmp == NULL)
+		return (-1);
+
 	while (i < size)
 	{
 		if (cmp(array[i]))
 			return (i);
 		i++;
 	}
-	return (-1);
+	return (-1); /*if no match found */
 }
