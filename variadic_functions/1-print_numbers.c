13db431c04100d48c4a819f847e21d2842a0e39c
lily <hueduong288@gmail.com>
2025-03-18 05:34:33 +0000

    print number arguments followed by a new line - make betty happy

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -10,28 +10,26 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-    va_list args;
-    unsigned int i;
+	va_list args;
+	unsigned int i;
 
-    va_start(args, n);
+	va_start(args, n);
 
-    if (separator == NULL)
-        return;
+	for (i = 0; i < n; i++)
+       	{
+		int x = va_arg(args, int);
 
-    for (i = 0; i < n; i++)
-    {
-        int x = va_arg(args, int);
-        printf("%d", x);
+		printf("%d", x);
 
-        if (separator != NULL && i < n-1)
-        {
-            printf("%s", separator);
-        }
-    }
+		if (separator != NULL && i < n-1)
+		{
+			printf("%s", separator);
+		}
+	}
 
-    printf("\n");
+	printf("\n");
 
-    va_end(args);
+	va_end(args);
 
 }
 
