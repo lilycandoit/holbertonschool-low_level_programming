6d06e80c2c6367a263fd2e5fa2bf1036b401f92e
lily <hueduong288@gmail.com>
2025-03-18 06:19:45 +0000

    print strings with variadic function - make betty happy

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -13,26 +13,26 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i;
-	
+
 	va_start(args, n);
-	
+
 	for (i = 0; i < n; i++)
 	{
 		char *x = va_arg(args, char *);
-		
+
 		if (x == NULL)
 			printf("(nil)");
 		else
 			printf("%s", x);
-		
+
 		if (separator != NULL && i < n-1)
 		{
-			printf("%s", separator)
+			printf("%s", separator);
 		}
 	}
-	
+
 	printf("\n");
-	
+
 	va_end(args);
 
 }
