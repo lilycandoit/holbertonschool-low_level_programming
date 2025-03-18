58d5e2fa553adb07fe3e1fe6ca37257bb8238f07
lily <hueduong288@gmail.com>
2025-03-18 04:34:42 +0000

    create variadic funciton to get sum of arguments - make betty happy

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -15,18 +15,19 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 
 	va_start(args, n);
-	
+
 	if (n == 0)
 		return (0);
-	
+
 	for (i = 0; i < n; i++)
 	{
 		int x = va_arg(args, int);
+
 		sum = sum + x;
 	}
-	
+
 	va_end(args);
-	
+
 	return (sum);
 
 }
