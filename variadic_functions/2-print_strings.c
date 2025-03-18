466e8b8903e909c33a84918705748c35796543a1
lily <hueduong288@gmail.com>
2025-03-18 06:17:20 +0000

    print strings followed by new line - make betty happy

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -13,27 +13,27 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i;
-
-    	va_start(args, n);
-
-    	for (i = 0; i < n; i++)
-    	{
-        	char *x = va_arg(args, char *);
-
-        	if (x == NULL)
-            		printf("(nil)");
+	
+	va_start(args, n);
+	
+	for (i = 0; i < n; i++)
+	{
+		char *x = va_arg(args, char *);
+		
+		if (x == NULL)
+			printf("(nil)");
 		else
-            		printf("%s", x);
-
-        	if (separator != NULL && i < n-1)
-        	{
-            		printf("%s", separator);
+			printf("%s", x);
+		
+		if (separator != NULL && i < n-1)
+		{
+			printf("%s", separator)
 		}
-    	}
-
-    	printf("\n");
-
-    	va_end(args);
+	}
+	
+	printf("\n");
+	
+	va_end(args);
 
 }
 
