9ba104aa71c15600ac95a7bb213bf796566f5a58
lily <hueduong288@gmail.com>
2025-03-20 12:25:05 +0000

    creat frint all function with struct type and function pointer - make betty happy

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,60 +1,56 @@
 #include<stdio.h>
 #include<stdarg.h>
 #include<string.h>
-
-/*struct for mapping format specifiers to functions */
-typedef struct format
-    {
-        char c;
-        void (*f)(va_list);
-    } ft;
+#include "variadic_functions.h"
 
 /* functions to print different types */
 void print_char(va_list args)
 {
-    printf("%c", va_arg(args, int));  /* in variadic function, 'char' is promoted to 'int' */
+	printf("%c", va_arg(args, int));
+	/* in variadic function, 'char' is promoted to 'int' */
 }
 
 void print_int(va_list args)
 {
-    printf("%d", va_arg(args, int));
+	printf("%d", va_arg(args, int));
 }
 
 void print_float(va_list args)
 {
-    printf("%f", va_arg(args, double));  /* in variadic function, 'float' is promoted to 'double' */
+	printf("%f", va_arg(args, double));
+	/* in variadic function, 'float' is promoted to 'double' */
 }
 
 void print_string(va_list args)
 {
-    char *s = va_arg(args, char *);
-    s = s ? s : "(nil)";
-    printf("%s", s);
+	char *s = va_arg(args, char *);
+	s = s ? s : "(nil)";
+	printf("%s", s);
 }
 
 /* function to get correct function pointer */
 void (*get_type(char c))(va_list)
 {
-    ft all_types[] = {
-        {'c', print_char},
-        {'i', print_int},
-        {'f', print_float},
-        {'s', print_string},
-        {'\0', NULL}
-    };
+	ft all_types[] = {
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_string},
+		{'\0', NULL}
+	};
 
-    int i = 0;
+	int i = 0;
 
-        while (all_types[i].c != '\0')
-        {
-            if (all_types[i].c == c)
-                return (all_types[i].f);
+	while (all_types[i].c != '\0')
+	{
+		if (all_types[i].c == c)
+		return (all_types[i].f);
 
-            i++;
+		i++;
 
-        }
+	}
 
-    return (NULL);
+	return (NULL);
 }
 
 /* the main print_all function  */
@@ -65,29 +61,29 @@ void (*get_type(char c))(va_list)
 */
 void print_all(const char * const format,  ...)
 {
-    va_list args;
-    unsigned int i = 0;
-    char *separator = "";
+	va_list args;
+	unsigned int i = 0;
+	char *separator = "";
 
-    va_start(args, format);
+	va_start(args, format);
 
-    while (format && format[i])
-    {
-        void (*func)(va_list) = get_type(format[i]);
+	while (format && format[i])
+	{
+	void (*func)(va_list) = get_type(format[i]);
 
-        if (func)
-        {
-            printf("%s", separator); /* print separator before next value */
-            func(args);
-            separator = ", "; /*update separator after 1 element */
-        }
-        i++;
+	if (func)
+	{
+		printf("%s", separator); /* print separator before next value */
+		func(args);
+		separator = ", "; /*update separator after 1 element */
+	}
+	i++;
 
-    }
+	}
 
-    printf("\n");  /* Print a newline at the end */
+	printf("\n");  /* Print a newline at the end */
 
-    va_end(args);  /* Clean up the va_list */
+	va_end(args);  /* Clean up the va_list */
 
 }
 
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -2,6 +2,13 @@
 #define MAIN_H
 #include <stddef.h>
 
+/*struct for mapping format specifiers to functions */
+typedef struct format
+{
+	char c;
+	void (*f)(va_list);
+} ft;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
