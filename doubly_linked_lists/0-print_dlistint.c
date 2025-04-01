5e5ffaa9ac8edad3e1f35d522997e4f4dafcd41d
lily <hueduong288@gmail.com>
2025-04-01 23:03:46 +0000

    Print all elements of the double linked list

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include "stdio.h"
+
+/**
+* print_dlistint - print all elements of double linked list
+* @h: the pointer to head of the double linked list
+* Return: the numbers of nodes in the list
+*/
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+	const dlistint_t *current_node = h;
+	/* to not modify the original h pointer */
+
+	while (current_node != NULL)
+	{
+
+		printf("%d\n", current_node->n);
+
+		count++;
+		current_node = current_node->next;
+		/* move to next node */
+	}
+
+	return (count);
+}
+
---
+++ doubly_linked_lists/README.md
@@ -0,0 +1,5 @@
+#**DOUBLY LINKED LIST**
+
+**There are 9 core tasks in this project**
+
+- Task1: Print all the elements of the double linked lists.
