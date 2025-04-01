94c6cb390633967c9cd3b55698a212fb71d16ed8
lily <hueduong288@gmail.com>
2025-04-01 01:21:57 +0000

    count the number of elements/nodes in the linked list

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include "stdio.h"
+
+/**
+* print_list - to count all elements
+* @h: the pointer to head of the linked list
+* Return: the number of nodes (elements)
+*/
+size_t list_len(const list_t *h)
+{
+	unsigned int node_count = 0;
+	const list_t *current = h;
+	/* create a temporary pointer to not modifying the original h pointer */
+
+	while (current != NULL)
+	{
+		node_count++;
+		current = current->next;
+	}
+
+	return (node_count);
+}
