b72b2d2e6a2ecb5775e02422de49418c21fa33fa
lily <hueduong288@gmail.com>
2025-04-02 03:04:27 +0000

    add node to the end of the list - fix format error

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -37,8 +37,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	while (temp->next != NULL)
 	{
 		temp = temp->next;
-		/* increase step, move to
-		 the next node*/
+		/* increase step, move to the next node*/
 	}
 	temp->next = new_node;
 	/* link the last node to new_node */
