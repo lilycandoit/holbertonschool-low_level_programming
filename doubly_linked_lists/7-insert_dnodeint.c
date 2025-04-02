576110216448a4c377d5c854dc7b1b2b4eda8667
lily <hueduong288@gmail.com>
2025-04-02 07:40:31 +0000

    shrink the function less than 40 lines of code

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -14,9 +14,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *insert_node, *node = *h; /* current node, and to save the head */
 	unsigned int i = 0;
 
-	insert_node = malloc(sizeof(dlistint_t));
-
-	if (insert_node == NULL)
+	if(!(insert_node = malloc(sizeof(dlistint_t))))
 		return (NULL);
 
 	insert_node->n = n;
@@ -38,9 +36,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		if (i == idx - 1)
 		{
 			insert_node->prev = node; /* link insert_node to previous node */
-			insert_node->next = node->next; /* link insert_node to the original next node */
+			insert_node->next = node->next;
+			/* link insert_node to the original next node */
 			if (node->next != NULL)
-				node->next->prev = insert_node; /* link orignal next node back to insert_node */
+				node->next->prev = insert_node;
+			/* link orignal next node back to insert_node */
 			node->next = insert_node; /* link previous node to insert_node */
 			return (insert_node);
 		}
