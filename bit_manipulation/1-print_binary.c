a349dacf4f7bba20249ce7b16e0d359c672392ad
lily <hueduong288@gmail.com>
2025-04-03 08:13:07 +0000

    make Betty happy - shorten the comment

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -27,7 +27,8 @@ void print_binary(unsigned long int n)
 
 	/* n & 1 is like getting the remainder when divived by 2 */
 	remainder = n & 1; /* n is updated from the last recursion */
-	/* odd number always has the last bit is 1 and even number has last bit is 0 */
+	/* odd number always has the last bit is 1 */
+	/* and even number has last bit is 0 */
 
 	_putchar(remainder + '0'); /* make it char type */
 }
