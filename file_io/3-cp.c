7443ec4ee21c2b4545e83b2669974947086d0ada
lily <hueduong288@gmail.com>
2025-04-07 04:09:32 +0000

    pending test

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -9,36 +9,20 @@
 void check_args(int argc);
 int open_file_from(const char *filename);
 int open_file_to(const char *filename);
-void copy_content(int fd_from, int fd_to, const char *file_from, const char *file_to);
+void copy_content(int fd_from, const char *file_from, const char *file_to);
 void close_file(int fd);
 
 int main(int argc, char *argv[])
 {
-    int fd_from, fd_to;
-    ssize_t test_read;
-    char buffer[1]; 
-    /* Just 1 byte to check readability */
+    int fd_from;
 
     check_args(argc);
 
     fd_from = open_file_from(argv[1]);
 
-    /* Check if the file is readable before doing anything else */
-    test_read = read(fd_from, buffer, 1);
-    if (test_read == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-        close_file(fd_from); /* Close if already opened */
-        exit(98);
-    }
-    lseek(fd_from, 0, SEEK_SET); /* Reset file pointer to start */
-
-    fd_to = open_file_to(argv[2]);
-
-    copy_content(fd_from, fd_to, argv[1], argv[2]);
+    copy_content(fd_from, argv[1], argv[2]);
 
     close_file(fd_from);
-    close_file(fd_to);
 
     return 0;
 }
@@ -76,14 +60,27 @@ int open_file_to(const char *filename)
     return fd;
 }
 
-void copy_content(int fd_from, int fd_to, const char *file_from, const char *file_to)
+void copy_content(int fd_from, const char *file_from, const char *file_to)
 {
     char buffer[BUFFER_SIZE];
-    ssize_t bytes_read, bytes_written;
+    ssize_t bytes_read, bytes_written, total_written;
+    int fd_to;
+
+    /* Try reading first */
+    bytes_read = read(fd_from, buffer, 1024);
+    if (bytes_read == -1)
+    {
+	    dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+	    exit(98);
+    }
+
+    /* only open the destionation file if reading worked */
+    fd_to = open_file_to(file_to);
 
     while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
     {
-        ssize_t total_written = 0;
+        total_written = 0;
+
         while (total_written < bytes_read)
         {
             bytes_written = write(fd_to, buffer + total_written, bytes_read - total_written);
@@ -101,6 +98,8 @@ void copy_content(int fd_from, int fd_to, const char *file_from, const char *fil
         dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
         exit(98);
     }
+
+    close_file(fd_to);
 }
 
 void close_file(int fd)
