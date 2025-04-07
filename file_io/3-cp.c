0782178f29ebab071c92248b9241c6575ce1a894
lily <hueduong288@gmail.com>
2025-04-07 04:56:40 +0000

    fix styles and docs for Betty

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -3,6 +3,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include "main.h"
 
 #define BUFFER_SIZE 1024
 
@@ -15,8 +16,6 @@
  */
 void check_args(int argc);
 int open_file_from(const char *filename);
-int open_file_to(const char *filename);
-void copy_content(int fd_from, const char *file_from, const char *file_to);
 void close_file(int fd);
 
 int main(int argc, char *argv[])
@@ -31,7 +30,7 @@ int main(int argc, char *argv[])
 
 	close_file(fd_from);
 
-	return 0;
+	return (0);
 }
 
 /**
@@ -58,95 +57,14 @@ void check_args(int argc)
 int open_file_from(const char *filename)
 {
 	int fd = open(filename, O_RDONLY);
+
 	if (fd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
 		exit(98);
 	}
 
-	return fd;
-}
-
-/**
- * open_file_to - opens a file for writing
- * @filename: name of the file to open
- *
- * Return: file descriptor of the opened file
- */
-int open_file_to(const char *filename)
-{
-	int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
-		exit(99);
-	}
-
-	return fd;
-}
-
-/**
- * copy_content - copies content from one file to another
- * @fd_from: file descriptor of the source file
- * @file_from: name of the source file
- * @file_to: name of the destination file
- *
- * Return: void
- */
-void copy_content(int fd_from, const char *file_from, const char *file_to)
-{
-	char buffer[BUFFER_SIZE];
-	ssize_t bytes_read, bytes_written, total_written;
-	int fd_to;
-
-	/* Try reading first */
-	bytes_read = read(fd_from, buffer, 1024);
-	if (bytes_read == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
-
-	/* only open the destionation file if reading worked */
-	fd_to = open_file_to(file_to);
-
-	/*  Write the first read chunk */
-	total_written = 0;
-	while (total_written < bytes_read)
-	{
-		bytes_written = write(fd_to, buffer + total_written, bytes_read - total_written);
-		if (bytes_written == -1)
-		{
-				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-				exit(99);
-		}
-		total_written += bytes_written;
-	}
-
-	/* Continue with the rest of the file */
-	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
-	{
-		total_written = 0;
-
-		while (total_written < bytes_read)
-		{
-			bytes_written = write(fd_to, buffer + total_written, bytes_read - total_written);
-			if (bytes_written == -1)
-			{
-				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-				exit(99);
-			}
-			total_written += bytes_written;
-		}
-	}
-
-	if (bytes_read == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
-
-	close_file(fd_to);
+	return (fd);
 }
 
 /**
--- file_io/main.h
+++ file_io/main.h
@@ -6,6 +6,9 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void copy_content(int fd_from, const char *file_from, const char *file_to);
+void close_file(int fd);
+
 
 #endif
 
